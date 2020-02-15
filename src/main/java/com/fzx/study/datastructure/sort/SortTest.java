efd6873225833d8e3e015a19fbe6d2aa99e584f0
fzx <fzx_java@163.com>
2020-02-15 23:28:47 +0800

    sort test

--- src/main/java/com/fzx/study/datastructure/sort/SortTest.java
+++ src/main/java/com/fzx/study/datastructure/sort/SortTest.java
@@ -27,26 +27,24 @@ public class SortTest {
              3 8 -2 2 -99 11
          */
         //bubbleSort(arr); //冒泡排序
-        //improvedBubbleSort(arr); //改进冒泡排序
-        // selectSort(arr);//选择排序
-        //insertSort(arr);
-        shellSort2(arr);
+        //bubbleSortPlus(arr); //改进冒泡排序
+        //selectSort(arr);//选择排序
+        //insertSort(arr); //插入排序
+        //shellSort(arr);  //希尔排序 互换法
+        shellSort2(arr);   //希尔排序 插值法
         System.out.println(Arrays.toString(arr));
 
 
     }
 
     public static void bubbleSort(int arr[]) {
-
-        int c = 0;
         int temp = 0;
-        for (int j = 0; j < arr.length; j++) {
-            for (int i = 0; i < arr.length - 1 - j; i++) {
-                System.out.println("比较次数" + c++);
-                if (arr[i] > arr[i + 1]) {
-                    temp = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
@@ -57,38 +55,51 @@ public class SortTest {
      *
      * @param arr
      */
-    public static void improvedBubbleSort(int arr[]) {
-        int c = 0;
+    public static void bubbleSortPlus(int arr[]) {
         boolean flag = true;
         int temp = 0;
-        for (int j = 0; j < arr.length; j++) {
-            for (int i = 0; i < arr.length - 1 - j; i++) {
-                System.out.println("比较次数" + c++);
-                if (arr[i] > arr[i + 1]) {
-                    temp = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = temp;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                     flag = false;
                 }
+
             }
             if (flag) {
                 break;
+            } else {
+                flag = true;
             }
         }
     }
 
     public static void selectSort(int arr[]) {
+        int minIndex = 0;
+        int minValue = 0;
 
-        int temp = 0;
         for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] > arr[j]) {
-                    temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+
+            minIndex = i;
+            minValue = arr[i];
+            for (int j = i; j < arr.length; j++) {
+
+                if (minValue > arr[j]) {
+                    minIndex = j;
+                    minValue = arr[j];
                 }
             }
+
+            if (i != minIndex) {
+                arr[minIndex] = arr[i];
+                arr[i] = minValue;
+            }
         }
+
+
     }
 
     /**
@@ -97,7 +108,7 @@ public class SortTest {
      * @param arr
      */
     public static void insertSort(int arr[]) {
-        int insertIndex = -1;
+        int insertIndex = 0;
         int insertValue = 0;
 
         for (int i = 1; i < arr.length; i++) {
@@ -107,51 +118,57 @@ public class SortTest {
                 arr[insertIndex + 1] = arr[insertIndex];
                 insertIndex--;
             }
-
-            if (insertIndex != i) {
+            if (insertIndex + 1 != i) {
                 arr[insertIndex + 1] = insertValue;
             }
+
         }
     }
 
 
     public static void shellSort(int arr[]) {
+        int temp = 0;
+
         for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+
             for (int i = gap; i < arr.length; i++) {
                 for (int j = i - gap; j >= 0; j -= gap) {
                     if (arr[j] > arr[j + gap]) {
-                        int temp = arr[j];
+                        temp = arr[j];
                         arr[j] = arr[j + gap];
                         arr[j + gap] = temp;
                     }
                 }
+
             }
         }
+
     }
 
     public static void shellSort2(int arr[]) {
 
+        int insertIndex = 0;
+
+        int insertValue = 0;
+
         for (int gap = arr.length / 2; gap > 0; gap /= 2) {
-            // 从第gap个元素，逐个对其所在的组进行直接插入排序
             for (int i = gap; i < arr.length; i++) {
-                int j = i;
-                int temp = arr[j];
-                if (arr[j] < arr[j - gap]) {
-                    while (j - gap >= 0 && temp < arr[j - gap]) {
-                        //移动
-                        arr[j] = arr[j - gap];
-                        j -= gap;
-                    }
-                    //当退出while后，就给temp找到插入的位置
-                    arr[j] = temp;
+                insertIndex = i - gap;
+                insertValue = arr[i];
+                while (insertIndex >= 0 && insertValue < arr[insertIndex]) {
+                    arr[insertIndex + gap] = arr[insertIndex];
+                    insertIndex -= gap;
                 }
 
+                if (insertIndex + gap != i) {
+                    arr[insertIndex + gap] = insertValue;
+                }
             }
-
         }
+
     }
 
-    public static void quickSort(int arr[],int left,int right){
+    public static void quickSort(int arr[], int left, int right) {
 
     }
 
